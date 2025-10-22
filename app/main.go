8276529417692f257459912b4cecea1b75e77c1e
MathPelicer <matheuspelicer58@gmail.com>
2025-10-21 22:49:31 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,9 +21,16 @@ const API_VERSION = "ApiVersion"
 const CORRELATION_ID = "CorrelationId"
 const MESSAGE_SIZE = "MessageSize"
 const ERROR_CODE = "ErrorCode"
+const LENGTH = "Length"
+const MIN_VERSION = "MinVersion"
+const MAX_VERSION = "MaxVersion"
+const THROTTLE_TIME = "ThrottleTime"
+const TAG_BUFFER = "TagBuffer"
+
 const MAX_SUPPORTED_VERSION = 4
 
 var RequestProperties = map[string]PropertyPositioning{
+	MESSAGE_SIZE:   {0, 4},
 	API_KEY:        {4, 6},
 	API_VERSION:    {6, 8},
 	CORRELATION_ID: {8, 12},
@@ -33,6 +40,12 @@ var ResponseProperties = map[string]PropertyPositioning{
 	MESSAGE_SIZE:   {0, 4},
 	CORRELATION_ID: {4, 8},
 	ERROR_CODE:     {8, 10},
+	LENGTH:         {10, 11},
+	API_KEY:        {11, 13},
+	MIN_VERSION:    {13, 15},
+	MAX_VERSION:    {15, 17},
+	THROTTLE_TIME:  {17, 21},
+	TAG_BUFFER:     {21, 22},
 }
 
 var ErrorCodes = map[string]int{}
@@ -72,19 +85,42 @@ func handleConnection(conn net.Conn) {
 		os.Exit(1)
 	}
 
-	var _ = parseRequestDataUint16(reqBuff, API_KEY)
+	var _ = parseRequestDataUint32(reqBuff, MESSAGE_SIZE)
+	var apiKey = parseRequestDataUint16(reqBuff, API_KEY)
 	var apiVersion = parseRequestDataUint16(reqBuff, API_VERSION)
 	var correlationId = parseRequestDataUint32(reqBuff, CORRELATION_ID)
+	//var _ = parseRequestDataUint16(reqBuff, LENGTH)
 	var errorCode = 0
 
 	if apiVersion > MAX_SUPPORTED_VERSION {
 		errorCode = 35
 	}
 
-	var buff = make([]byte, 10)
-	createResponseDataUint32(buff, MESSAGE_SIZE, 0)
+	var apiVersionsLenght = 1
+
+	if apiKey != 0 {
+		apiVersionsLenght += 1
+	}
+
+	var buff = make([]byte, 23)
+	var bytesSize = 1
 	createResponseDataUint32(buff, CORRELATION_ID, correlationId)
+	bytesSize += 4
 	createResponseDataUint16(buff, ERROR_CODE, uint16(errorCode))
+	bytesSize += 2
+	createResponseDataUint8(buff, LENGTH, uint8(apiVersionsLenght))
+	bytesSize += 1
+	createResponseDataUint16(buff, API_KEY, uint16(apiKey))
+	bytesSize += 2
+	createResponseDataUint16(buff, MIN_VERSION, uint16(0))
+	bytesSize += 2
+	createResponseDataUint16(buff, MAX_VERSION, uint16(MAX_SUPPORTED_VERSION))
+	bytesSize += 2
+	createResponseDataUint32(buff, THROTTLE_TIME, 0)
+	bytesSize += 4
+	createResponseDataUint8(buff, TAG_BUFFER, uint8(0))
+	bytesSize += 1
+	createResponseDataUint32(buff, MESSAGE_SIZE, uint32(bytesSize))
 	conn.Write(buff)
 
 	defer conn.Close()
@@ -104,4 +140,8 @@ func createResponseDataUint32(buff []byte, property string, data uint32) {
 
 func createResponseDataUint16(buff []byte, property string, data uint16) {
 	binary.BigEndian.PutUint16(buff[ResponseProperties[property].Start:ResponseProperties[property].End], data)
+}
+
+func createResponseDataUint8(buff []byte, property string, data uint8) {
+	binary.PutUvarint(buff[ResponseProperties[property].Start:ResponseProperties[property].End], uint64(data))
 }
